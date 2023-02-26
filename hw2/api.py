48d092f4fdc1905151eff45e41b458973ae7c3b4
Albert Skalt <allber971@gmail.com>
2023-02-26 20:16:01 +0300

    hw2: fix prog task3

--- hw2/api.py
+++ hw2/api.py
@@ -1,17 +1,20 @@
 import typing
-from fastapi import FastAPI, HTTPException
+import base64
+from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi.responses import Response
 from pydantic import BaseModel
+from io import BytesIO
 
 
 class Product(BaseModel):
     id: typing.Union[str, None]
     name: str
     description: typing.Union[str, None]
-    image: typing.Union[bytes, None]
 
 
 app = FastAPI()
 products: typing.Dict[int, Product] = {}
+images: typing.Dict[int, bytes] = {}
 free_id = 0
 
 
@@ -36,7 +39,26 @@ def add_product_item(item: Product):
     return item
 
 
-@app.put("/products/{product_id}", status_code=201)
+@app.post("/products/{product_id}/image", status_code=201)
+async def upload_product_image(product_id: int, image: UploadFile):
+    if product_id not in products:
+        raise HTTPException(status_code=404, detail="Product not found")
+    data = await image.read()
+    images[product_id] = base64.b64encode(data)
+    return {"status": "success"}
+
+
+@app.get("/products/{product_id}/image", status_code=200, response_class=Response)
+def load_product_image(product_id: int):
+    if product_id not in products:
+        raise HTTPException(status_code=404, detail="Product not found")
+    if product_id not in images:
+        raise HTTPException(status_code=404, detail="Product hasn't image")
+    data = base64.b64decode(images[product_id])
+    return Response(content=data, media_type="image/png")
+
+
+@ app.put("/products/{product_id}", status_code=201)
 def update_product_item(product_id: int, item: Product):
     if product_id not in products:
         raise HTTPException(status_code=404, detail="Product not found")
@@ -46,7 +68,7 @@ def update_product_item(product_id: int, item: Product):
     return item
 
 
-@app.delete("/products/{product_id}", status_code=201)
+@ app.delete("/products/{product_id}", status_code=201)
 def remove_product_item(product_id: int):
     if product_id not in products:
         raise HTTPException(status_code=404, detail="Product not found")
